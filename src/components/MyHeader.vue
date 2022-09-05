131a3690aecad39a8e4bcd843b701b040cf90e67
ariannadepasquale <depasqualearianna@gmail.com>
2022-09-05 16:44:17 +0200

    Init Project & components, install fontAw & BStrap

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-warning">header</h1>
+</template>
+
+<script>
+export default {
+    name:'MyHeader'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Main</h2>
+</template>
+
+<script>
+export default {
+    name:'MyMain'
+
+}
+</script>
+
+<style>
+
+</style>
