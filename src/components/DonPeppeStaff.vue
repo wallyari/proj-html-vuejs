409b331bb8807bfecce36d92ca167a26a4213bd4
ariannadepasquale <depasqualearianna@gmail.com>
2022-09-06 16:42:43 +0200

    Add section "Logo Clients" in DonPeppeStaff comp

--- src/components/DonPeppeStaff.vue
+++ src/components/DonPeppeStaff.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="Staff">       
+    <div class="Staff pt-2">       
     <div class="img-staff">
         <img src="../assets/img/h1-team-1a-700x700.jpg" alt="staff_1">
         <img src="../assets/img/h1-team-2a.jpg" alt="staff_2">
         <img src="../assets/img/h1-team-3a.jpg" alt="staff_3">
         <img src="../assets/img/h1-team-4a.jpg" alt="staff_4">
+    </div>
+    <!-- Logo Clients -->
+    <div class="clients pt-2">
+        <div class="logo-clients">
+            <img src="../assets/img/h1-clients-img-4.png" alt="clients_1">
+            <img src="../assets/img/h1-clients-img-3.png" alt="clients_2">
+            <img src="../assets/img/h1-clients-img-1.png" alt="clients_3">
+            <img src="../assets/img/h1-clients-img-2.png" alt="clients_4">
+            <img src="../assets/img/h1-clients-img-5.png" alt="clients_5">
+        </div>
     </div>
     </div>
 </template>
@@ -23,7 +33,7 @@ export default {
 
 @import '../assets/style/variables.scss';
 
-
+//Don Peppe Staff
 .img-staff{
     display: flex;
     
@@ -33,7 +43,17 @@ export default {
 }
 }
 
-
-
+//Clients Logo
+.clients{
+    width: 100vw;
+    height: 200px;
+    background-color: $background-clients;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.logo-clients img{
+    width: calc(100% / 5);
+}
 
 </style>
